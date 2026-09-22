8d9f3a412f5cf88c01a97a79896d6f4484c7c9d2
cmrh-jim <cmrh.jim@gmail.com>
2026-09-22 17:22:08 +0600

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,9 @@
-import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DIRECT_DATABASE_URL'),  // Use the direct (5432) URL for migrations
+    url: env('DIRECT_DATABASE_URL'),
   },
-});
+})
